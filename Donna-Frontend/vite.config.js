fe70b4c13d1a75dda61263b414559c0c06607d04
parisha6 <156088678+parisha6@users.noreply.github.com>
2024-12-10 22:45:25 -0500

    Update vite.config.js

--- Donna-Frontend/vite.config.js
+++ Donna-Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Makes the server accessible from external sources (important for deployment)
+    port: 5173,  
+    }
 })
